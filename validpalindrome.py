76a4a9974a7edc2f78c2fc90ca2b2701f93dc17a
Alvisha-28 <alvishaveigas@gmail.com>
2026-05-28 15:27:03 +0530

    updated files

--- validpalindrome.py
+++ validpalindrome.py
@@ -1,11 +1,14 @@
-#palindrome and print the string in reverse order
-def is_palindrome(s):
-    cleaned_s = ''.join(s.split()).lower()
-    return cleaned_s == cleaned_s[::-1]
-input_string = input("Enter a string: ")
-if is_palindrome(input_string):
-    print(f'"{input_string}" is a palindrome.')
-else:
-    print(f'"{input_string}" is not a palindrome.')
-    print(f'Reversed: "{input_string[::-1]}"')
+# #palindrome and print the string in reverse order
+# def is_palindrome(s):
+#     cleaned_s = ''.join(s.split()).lower()
+#     return cleaned_s == cleaned_s[::-1]
+# input_string = input("Enter a string: ")
+# if is_palindrome(input_string):
+#     print(f'"{input_string}" is a palindrome.')
+# else:
+#     print(f'"{input_string}" is not a palindrome.')
+#     print(f'Reversed: "{input_string[::-1]}"')
     
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
